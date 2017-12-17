a8bdd2e91d022ed01661c41e360b4d6ed3b3f9dc
Stuart Taylor <staylor1012@gmail.com>
2017-12-17 08:46:42 +0000

    Whitelist netlify domains

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -8,8 +8,12 @@ const whitelist = [
   'http://localhost:8000' // guides dev
 ];
 
+const netlifyRE = /\.netlify\.com/;
+
 function isTrustedOrigin(origin, callback) {
   if (whitelist.indexOf(origin) !== -1) {
+    callback(null, true);
+  } else if (netlifyRE.test(origin)) {
     callback(null, true);
   } else {
     callback(new Error('The request is not from an authorised origin'));
